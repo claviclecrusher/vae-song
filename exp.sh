327144d617d7acf70c0fc5213852fc9bc10e5644
song <clavicle.shatter@gmail.com>
2025-08-08 16:35:30 +0900

    exp setting

---
+++ exp.sh
@@ -0,0 +1,7 @@
+python run_vis_lip_kl_exp.py --alpha 0.1 --K 16 --K_z 16 --z_min -3 --z_max 3 --device cuda:0 --output_dir ./results/vis_lip_kl_exp/alpha_0.1
+
+python run_vis_lip_kl_exp.py --alpha 0.2 --K 16 --K_z 16 --z_min -3 --z_max 3 --device cuda:0 --output_dir ./results/vis_lip_kl_exp/alpha_0.2
+
+python run_vis_lip_kl_exp.py --alpha 0.3 --K 16 --K_z 16 --z_min -3 --z_max 3 --device cuda:0 --output_dir ./results/vis_lip_kl_exp/alpha_0.3
+
+python run_vis_lip_kl_exp.py --alpha 0.4 --K 16 --K_z 16 --z_min -3 --z_max 3 --device cuda:0 --output_dir ./results/vis_lip_kl_exp/alpha_0.4
