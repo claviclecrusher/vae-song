59797b88a919021acc0d3f5f1e6e974b4323c3d0
song <clavicle.shatter@gmail.com>
2025-08-08 17:15:38 +0900

    exp setting

--- exp.sh
+++ exp.sh
@@ -1,19 +1,10 @@
-python run_vis_lip_kl_exp.py --alpha 0.0 --beta 0.1 --K 16 --K_z 16 --z_min -3 --z_max 3 --device cuda:0 --output_dir ./results/vis_lip_kl_exp/alpha_0.0
+alphas=(0.0 0.01 0.001 0.0001 0.1 0.2 0.3 0.4 1.0)
+betas=(0.1 0.2 1.0)
 
-python run_vis_lip_kl_exp.py --alpha 0.01 --beta 0.1 --K 16 --K_z 16 --z_min -3 --z_max 3 --device cuda:0 --output_dir ./results/vis_lip_kl_exp/alpha_0.01
-
-python run_vis_lip_kl_exp.py --alpha 0.001 --beta 0.1 --K 16 --K_z 16 --z_min -3 --z_max 3 --device cuda:0 --output_dir ./results/vis_lip_kl_exp/alpha_0.001
-
-python run_vis_lip_kl_exp.py --alpha 0.0001 --beta 0.1 --K 16 --K_z 16 --z_min -3 --z_max 3 --device cuda:0 --output_dir ./results/vis_lip_kl_exp/alpha_0.0001
-
-
-python run_vis_lip_kl_exp.py --alpha 0.1 --beta 0.1 --K 16 --K_z 16 --z_min -3 --z_max 3 --device cuda:0 --output_dir ./results/vis_lip_kl_exp/alpha_0.1
-
-python run_vis_lip_kl_exp.py --alpha 0.2 --beta 0.1 --K 16 --K_z 16 --z_min -3 --z_max 3 --device cuda:0 --output_dir ./results/vis_lip_kl_exp/alpha_0.2
-
-python run_vis_lip_kl_exp.py --alpha 0.3 --beta 0.1 --K 16 --K_z 16 --z_min -3 --z_max 3 --device cuda:0 --output_dir ./results/vis_lip_kl_exp/alpha_0.3
-
-python run_vis_lip_kl_exp.py --alpha 0.4 --beta 0.1 --K 16 --K_z 16 --z_min -3 --z_max 3 --device cuda:0 --output_dir ./results/vis_lip_kl_exp/alpha_0.4
-
-
-python run_vis_lip_kl_exp.py --alpha 1.0 --beta 0.1 --K 16 --K_z 16 --z_min -3 --z_max 3 --device cuda:0 --output_dir ./results/vis_lip_kl_exp/alpha_1.0
+for beta in "${betas[@]}"
+do
+  for alpha in "${alphas[@]}"
+  do
+    python run_vis_lip_kl_exp.py --alpha $alpha --beta $beta --K 16 --K_z 16 --z_min -3 --z_max 3 --device cuda:0 --output_dir ./results/vis_lip_kl_exp/alpha_${alpha}_beta_${beta}
+  done
+done
